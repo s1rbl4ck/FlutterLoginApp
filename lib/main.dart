abc0e2df44052b2253977593a38250e6782f68a6
s1rbl4ck <danesh.hb82@gmail.com>
2022-08-13 10:38:56 +0430

    Login Page has been done

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,227 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// Created By @s1rbl4ck
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login App',
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: SizedBox(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 200,
+                  left: -100,
+                  child: Container(
+                    width: 300,
+                    height: 300,
+                    decoration: const BoxDecoration(
+                      color: Color(0x304599ff),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(150),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  right: -10,
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Color(0x30cc33ff),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(100),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 80,
+                      sigmaY: 80,
+                    ),
+                    child: Container(),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 70,
+                        ),
+                        _logo(),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        _loginLabel(),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        _labelTextInput("Email", "email@example.com", false),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        _labelTextInput("Password", "********", true),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        _loginBtn(),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        _signUpLabel(
+                          "Don't have an account yet?",
+                          const Color(0xff909090),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        _signUpLabel(
+                          "Sign Up",
+                          const Color(0xff164276),
+                        ),
+                        const SizedBox(
+                          height: 90,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget _signUpLabel(String label, Color textColor) {
+  return Text(
+    label,
+    style: GoogleFonts.josefinSans(
+      textStyle: TextStyle(
+        color: textColor,
+        fontWeight: FontWeight.w800,
+        fontSize: 18,
+      ),
+    ),
+  );
+}
+
+Widget _loginBtn() {
+  return Container(
+    width: double.infinity,
+    height: 60,
+    decoration: const BoxDecoration(
+      color: Color(0xff008fff),
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    ),
+    child: TextButton(
+      onPressed: () {
+        Get.snackbar(
+          'Successfull Log In',
+          'You have successfully logged in.',
+          overlayBlur: 0,
+          backgroundColor: Colors.greenAccent,
+          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+        );
+      },
+      child: Text(
+        "Login",
+        style: GoogleFonts.josefinSans(
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _labelTextInput(String label, String hintText, bool isPassword) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: GoogleFonts.josefinSans(
+          textStyle: const TextStyle(
+            color: Color(0xff8fa1b6),
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      TextField(
+        obscureText: isPassword,
+        cursorColor: Colors.cyanAccent[300],
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: Color(0xffc5d2e1),
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+          enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xffdfe8f3),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _loginLabel() {
+  return Center(
+    child: Text(
+      "Login",
+      style: GoogleFonts.josefinSans(
+        textStyle: const TextStyle(
+          color: Color(0xff191919),
+          fontWeight: FontWeight.w900,
+          fontSize: 34,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _logo() {
+  return Center(
+    child: SizedBox(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Image.network(
+            "https://avatars.githubusercontent.com/u/51126010?v=4"),
+      ),
+      height: 80,
+    ),
+  );
 }
